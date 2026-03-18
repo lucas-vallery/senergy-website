b00dd8c694dd3ba05aa1b1e58e5602a1dc52d7b3
lucas-vallery <lucas-vallery@users.noreply.github.com>
2026-03-18 20:57:19 +0000

    deploy: e3b642e2828be0f383dad877e11041d6ef2e8986

---
+++ _next/static/chunks/app/[locale]/page-d8e13edaee1f5313.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[61],{},function(n){n.O(0,[234,625,523,971,117,744],function(){return n(n.s=3523)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/[locale]/sav/page-6d7960c61494b9eb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[696],{},function(n){n.O(0,[234,625,314,971,117,744],function(){return n(n.s=7314)}),_N_E=n.O()}]);
